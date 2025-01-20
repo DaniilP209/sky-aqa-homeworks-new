192d16a99185974d6b09794b85eb212c4ad54ddf
DaniilP209 <pd8v.qa@gmail.com>
2025-01-20 20:17:01 +0300

    first commit

---
+++ lesson_1_task_7.py
@@ -0,0 +1,41 @@
+def fun1():
+    print(1, end='')
+
+def fun2():
+    print(2, end='')
+
+def fun3():
+    print(3, end='')
+
+def fun4():
+    print(4, end='')
+
+def fun5():
+    print(5, end='')
+
+def fun6():
+    print(6, end='')
+
+def fun7():
+    print(7, end='')
+
+def fun8():
+    print(8, end='')
+
+def fun9():
+    print(9, end='')
+
+def fun0():
+    print(0, end='')
+
+fun8()
+fun8()
+fun0()
+fun0()
+fun5()
+fun5()
+fun5()
+fun3()
+fun5()
+fun3()
+fun5()
---
+++ lesson_1_task_8.py
@@ -0,0 +1,15 @@
+def fun(num):
+    print(num, end='')
+
+
+fun(8)
+fun(8)
+fun(0)
+fun(0)
+fun(5)
+fun(5)
+fun(5)
+fun(5)
+fun(3)
+fun(5)
+fun(3)
